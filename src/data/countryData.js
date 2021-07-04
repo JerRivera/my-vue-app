fc0917c3f8d0c309d52a71923c1d93fc75cc0883
BuildTools <unconfigured@null.spigotmc.org>
2021-07-04 23:57:36 +0200

    Initial Commit

---
+++ src/data/countryData.js
@@ -0,0 +1,46 @@
+const countryData = {
+    countries: [
+        {
+            id: 0,
+            name: 'Verenigde Staten (USA)',
+            capital: 'Washington',
+            cost: 1250,
+            details: 'De Verenigde Staten zijn een van de meeste bezochte landen ter wereld. Meest bekend om al hun fast food ketens. Hier kan je van alles doen en meer!',
+            img: 'washington.jpg'
+        },
+        {
+            id: 1,
+            name: 'Nederland',
+            capital: 'Amsterdam',
+            cost: 100,
+            details: 'De hoofdstad van Nederland. Hier heb je de beste feesten en lekkere buitenlands eten. Perfect voor het nachtleven!',
+            img: 'amsterdam.jpg'
+        },
+        {
+            id: 2,
+            name: 'België',
+            capital: 'Brussel',
+            cost: 500,
+            details: 'De hoofdstad van België. Mooie bezienswaardigheden als de oude Grote Markt, Manneken Pis en veel meer andere dingen! Ook bekend om hun beroemde patat.',
+            img: 'brussel.jpg'
+        },
+        {
+            id: 3,
+            name: 'Duitsland',
+            capital: 'Berlijn',
+            cost: 795,
+            details: 'Berlijn heeft heel veel geschiedenis gehad. Hier kan je bijvoorbeeld de Berlijns Muur bezoeken en de Brandenburger Tor dat hierboven te zien is.',
+            img: 'berlijn.jpg'
+        },
+        {
+            id: 4,
+            name: 'Rusland',
+            capital: 'Moskou',
+            cost: 990,
+            details: 'Moskou is de grootste stad uit heel de Europese Unie. Met hun unieke gebouwen op het Rode Plein kan je wel zeker merken dat het Rusland is.',
+            img: 'moskou.jpg'
+        }
+    ]
+}
+
+export default countryData;
